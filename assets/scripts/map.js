36b5234a749d0bf4ea7a5e732b08aa8274a42581
RaphaMX <161722373+RaphaMX@users.noreply.github.com>
2024-09-03 11:11:43 +0000

    first commit

---
+++ assets/scripts/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([23.2100, 47.5005], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
